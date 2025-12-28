b9b311a31bb8e28cfc359d884b8dee06341d35d6
mohammadhoseinbab <sirhb77@gmail.com>
2025-12-28 23:28:08 +0100

    adding tailwindcss to the project and some styling

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -7,7 +7,7 @@ import { useAuthStore } from "../Store/useAuthStore"
 
 export default function LoginPage() {
     const navigate = useNavigate()
-    const useAuth = useAuthStore((s)=>s.setAuth)
+    const useAuth = useAuthStore((s) => s.setAuth)
 
     const [email, setEmail] = useState<string>("")
     const [password, setPassword] = useState<string>("")
@@ -17,7 +17,7 @@ export default function LoginPage() {
     const loginMutation = useMutation<AuthReponse, Error, LoginRequest>({
         mutationFn: login,
         onSuccess: (res) => {
-            useAuth({user:res.user , accessToken : res.accessToken })
+            useAuth({ user: res.user, accessToken: res.accessToken })
             navigate("/dashboard")
         }, onError: (e) => {
             console.log(e.message)
@@ -35,15 +35,49 @@ export default function LoginPage() {
     return (
 
         <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="">Enter your Email</label>
-                <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
-                <label htmlFor="">Enter your password</label>
-                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
-                <button type="submit" disabled={loginMutation.isPending}>{loginMutation.isPending ? "logging in ..." : "Submit"}</button>
-            </form>
-
+                <form className="
+    mx-auto mt-6 sm:mt-10
+    w-full max-w-md
+    border-2 rounded-lg
+    p-4 sm:p-6
+    space-y-4
+  "
+                    onSubmit={handleSubmit}>
+                    <label className="block text-sm sm:text-base"
+                        htmlFor="">Enter your Email</label>
+                    <input
+                        className=" w-full
+        bg-gray-500 text-white placeholder:text-gray-300
+        focus:outline-none focus:ring-2 focus:ring-sky-400
+        rounded px-4 py-2
+      "
+                        placeholder="abs@mail.com"
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)} value={email} />
+                    <label className="block text-sm sm:text-base"
+                        htmlFor="">Enter your password</label>
+                    <input className="
+        w-full
+        bg-gray-500 text-white placeholder:text-gray-300
+        focus:outline-none focus:ring-2 focus:ring-sky-400
+        rounded px-4 py-2
+      "
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)} value={password} />
+                    <button className="
+      w-full
+      bg-sky-300 text-white font-bold
+      rounded
+      py-2
+      hover:bg-sky-700
+      disabled:opacity-60 disabled:cursor-not-allowed
+    "
+                        type="submit"
+                        disabled={loginMutation.isPending}>{loginMutation.isPending ? "logging in ..." : "Submit"}</button>
+                </form>
+         
             {loginMutation.isError && (<div>{loginMutation.error.message}</div>)}
+
         </>
     )
 }
--- src/Pages/SignupPage.tsx
+++ src/Pages/SignupPage.tsx
@@ -24,14 +24,16 @@ export default function SignupPage() {
     }
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="">Insert youe name :</label>
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                <label htmlFor="">Insert your email</label>
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <label htmlFor="">Insert your password</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit" disabled={mutationRegister.isPending}>
+            <form className="mx-auto max-w-md w-full mt-6 border-2 rounded-lg p-4 space-y-4" onSubmit={handleSubmit}>
+                <label className="block" htmlFor="">Insert youe name </label>
+                <input className="bg-gray-500 w-full placeholder:text-gray-300 text-white focus:outline-none px-4 py-2 focus:ring-2 focus:ring-sky-400 rounded"
+                    placeholder="your name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                <label className="block" htmlFor="">Insert your email</label>
+                <input className="bg-gray-500 w-full placeholder:text-gray-300 text-white focus:outline-none px-4 py-2 focus:ring-2 focus:ring-sky-400 rounded"
+                    placeholder="abs@mail.com" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <label className="block" htmlFor="">Insert your password</label>
+                <input className="bg-gray-500 w-full py-2 px-4 focus:outline-none text-white focus:ring-2 focus:ring-sky-400 rounded" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button className="w-full bg-sky-500 py-2 rounded text-white hover:bg-sky-700 font-bold  focus:outline-none   " type="submit" disabled={mutationRegister.isPending}>
                     {mutationRegister.isPending ? "Registering ...." : "Submit"}
                 </button>
             </form>
